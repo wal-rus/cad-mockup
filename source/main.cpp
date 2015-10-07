038da9a7d4719bdaefec302a7d1a44deca51740e
Walter Gray <wgray@leapmotion.com>
2015-10-07 01:46:43 -0700

    Basic working version

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
+#include <limits>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 #include "picojson.h"
 
@@ -30,6 +35,18 @@ struct Vector {
 
 Vector operator-(const Vector& v0, const Vector& v1) {
   return { v0.x - v1.x, v0.y - v1.y };
+}
+
+Vector operator+(const Vector& v0, const Vector& v1) {
+  return { v0.x + v1.x, v0.y + v1.y };
+}
+
+Vector operator*(const Vector& v0, const Vector& v1) {
+  return { v0.x * v1.x, v0.y * v1.y };
+}
+
+Vector operator/(const Vector& v0, const Vector& v1) {
+  return { v0.x / v1.x, v0.y / v1.y };
 }
 
 double Dot(const Vector& v0, const Vector& v1) {
@@ -106,6 +123,80 @@ double ComputePathCost(const ToolInfo& tool, const ToolPath& path) {
   return sumCost;
 };
 
+Vector ComputeRequiredDimensions(const MaterialInfo& material, const ToolPath& path) {
+  const auto& obj = path.v.get<picojson::object>();
+  const auto& edges = obj.find("Edges")->second.get<picojson::object>();
+  const auto& vertices = obj.find("Vertices")->second;
+  
+  Vector minPoint = { std::numeric_limits<double>::max(), std::numeric_limits<double>::max() };
+  Vector maxPoint = { std::numeric_limits<double>::min(), std::numeric_limits<double>::min() };
+  
+  for( const auto& edge : edges) {
+    const auto type = edge.second.get("Type").to_str();
+    if(type == "LineSegment") {
+      const auto& edgeVertices = edge.second.get("Vertices").get<picojson::array>();
+      const auto v0 = ParseVertex(vertices, edgeVertices[0].to_str());
+      const auto v1 = ParseVertex(vertices, edgeVertices[1].to_str());
+      minPoint.x = std::min(minPoint.x, std::min(v0.x,v1.x));
+      minPoint.y = std::min(minPoint.y, std::min(v0.y,v1.y));
+      maxPoint.x = std::max(maxPoint.x, std::max(v0.x,v1.x));
+      maxPoint.y = std::max(maxPoint.y, std::max(v0.y,v1.y));
+    }
+    else if( type == "CircularArc") {
+      const auto& edgeVertices = edge.second.get("Vertices").get<picojson::array>();
+      const auto center = ParseVector(edge.second.get("Center"));
+      
+      auto v0 = ParseVertex(vertices, edgeVertices[0].to_str());
+      auto v1 = ParseVertex(vertices, edgeVertices[1].to_str());
+
+      //Ensure that v0 is the first vertex on the arc, moving counter-clockwise.
+      if(edgeVertices[1] != edge.second.get("ClockwiseFrom"))
+        std::swap(v0,v1);
+
+      const auto radius = Distance(center,v0);
+      const auto arcLine0 = Normalized(v0 - center);
+      const auto arcLine1 = Normalized(v1 - center);
+      
+      //we only care about the 4 maximal points on the circle, or in the case of the arc,
+      //the ones that fit within the arc boundaries.
+      const auto a0 = atan2(arcLine0.y, arcLine0.x);
+      auto a1 = atan2(arcLine1.y, arcLine1.x);
+
+      //ensure a1 > a0 so that the clamping operation works correctly
+      if( a1 <= a0)
+        a1 += 2*M_PI;
+
+      //For each of the 4 cardinal directions...
+      for(int i = 0; i < 4; ++i) {
+        double theta = M_PI_2 * i;
+        theta = std::min( a1, std::max(a0, theta) ); //clamp between a0 and a1
+
+        const Vector angleVector = { cos(theta)*radius, sin(theta)*radius };
+        const Vector arcPoint = center + angleVector;
+      
+        minPoint.x = std::min(minPoint.x, arcPoint.x);
+        minPoint.y = std::min(minPoint.y, arcPoint.y);
+        maxPoint.x = std::max(maxPoint.x, arcPoint.x);
+        maxPoint.y = std::max(maxPoint.y, arcPoint.y);
+      }
+    }
+    else {
+      std::cout << "Unknown segment type:" << type << std::endl;
+    }
+  }
+  
+  return maxPoint - minPoint;
+}
+
+double ComputeMaterialCost(const MaterialInfo& mat, const ToolInfo& tool, const ToolPath& path) {
+  auto dimensions = ComputeRequiredDimensions(mat,path);
+  dimensions.x += tool.padding;
+  dimensions.y += tool.padding;
+  const auto area = dimensions.x * dimensions.y;
+  const auto cost = area * mat.cost_per_sq_in;
+  return cost;
+}
+
 int main(int argc, char** argv) {
   if(argc != 2) {
     PrintUsage();
@@ -125,9 +216,12 @@ int main(int argc, char** argv) {
 
   ToolPath path = {std::move(v)};
   
-  auto machineCost = ComputePathCost(LASER_CUTTER, path);
+  const auto machineCost = ComputePathCost(LASER_CUTTER, path);
   std::cout << "Machine cost is " << machineCost << std::endl;
   
+  const auto materialCost = ComputeMaterialCost(ALUMINUM, LASER_CUTTER, path);
+  std::cout << "Material cost is " << materialCost << std::endl;
 
+  std::cout << "Total cost is " << std::fixed << std::setprecision(2) << machineCost + materialCost << std::endl;
   return 0;
 }
